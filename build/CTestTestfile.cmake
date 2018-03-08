b854c55b47fc9fa0b6344b044327864109730aef
giammandro <andrea.giammanco@gmail.com>
2018-03-08 17:47:54 +0100

    remove current build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andrea/Desktop/tesi_ort/code/kdgi
-# Build directory: /Users/andrea/Desktop/tesi_ort/code/kdgi/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tomita_15 "/Users/andrea/Desktop/tesi_ort/code/kdgi/build/runUnitTests")
-subdirs("gi")

c08cdfb6d2654b942b33e0d38ab1105a4d41d9f5
giammandro <andrea.giammanco@gmail.com>
2018-05-17 17:38:56 +0200

    Added tests and all bindings. TODO: assignment operator, fix read_dfa_file()

--- py/gipy.cc
+++ py/gipy.cc
@@ -1,19 +1,80 @@
 #include <pybind11/pybind11.h>
 
+//For automatic conversions
+#include <pybind11/stl.h>
+#include <pybind11/complex.h>
+#include <pybind11/functional.h>
+#include <pybind11/chrono.h>
+
+//For operators overload: https://pybind11.readthedocs.io/en/stable/advanced/classes.html#operator-overloading
+#include <pybind11/operators.h>
+
 #include "dfa.h"
+
+//Default arguments: https://pybind11.readthedocs.io/en/stable/basics.html#default-args
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-    return i + j;
-}
-
 PYBIND11_MODULE(gi_gipy, m) {
-    m.doc() = "pybind11 plugin for the kdgi library"; // module docstring
+    m.doc() = "pybind11 plugin for the kdgi library";
 
-    m.def("addxxx", &add, "A function which adds two numbers",
-          py::arg("i"), py::arg("j"));
+    py::class_<Dfa>(m, "Dfa")
 
-    py::class_<Dfa>(m, "DFA")
-      .def(py::init());
+      //*** Constructors ***
+      .def(py::init(),"Make an instance of null dfa.")
+
+      .def(py::init<const Dfa&>(),"Constructor for make a copy of a dfa \"d1\".")
+
+      //===========================================================================================
+
+      //*** Operators overload ***
+      //.def(py::self = py::self, "Definition of assignement operator=.")
+
+      .def(py::self == py::self, "Definition of equivalence operator==.")
+
+      .def(py::self < py::self, "Definition of minor operator<. Return true if calling DFA has less states than DFA @p d1.")
+
+      //===========================================================================================
+
+      //*** Basic methods ***
+      .def("get_dim_alphabet", &Dfa::get_dim_alphabet,"Return size of alphabet.")
+
+      .def("get_alphabet",&Dfa::get_alphabet,"Return a vector with alphabet symbols.")
+
+      .def("get_accepting_states",&Dfa::get_accepting_states,"Return accepting states.")
+
+      .def("is_accepting",&Dfa::is_accepting,"Returns true if current state is accepting.")
+
+      .def("get_num_states",&Dfa::get_num_states,"Get number of states.")
+
+      .def("get_start_state",&Dfa::get_start_state,"Get index of start state.")
+
+      .def("print_dfa_ttable",&Dfa::print_dfa_ttable,"Print the transition table of current dfa. Before the transition table print the title passed as parameter.")
+      
+      .def_static("read_dfa_file", &Dfa::read_dfa_file, "Read a dfa from a file.")
+      
+      .def("save_dfa", &Dfa::print_dfa_in_text_file, "Print a DFA in a text file. Adopted format is the same used for reading a DFA from file.")
+      
+      .def("print_dfa_dot", &Dfa::print_dfa_dot, "Print a dot file for the current dfa.")
+      
+      .def("membership_query", &Dfa::membership_query, "Make a membership query to dfa with the \"phrase\" string with alphabet symbol. Return \"true\" if the arrive state for \"phrase\" is acceptor, else \"false\".")
+      
+      .def("gen_samples", &Dfa::generate_pos_neg_samples_without_weights, "It returns a set random samples (all different) generated from current DFA.")
+      
+      //===========================================================================================
+
+      //*** W-Method ***
+      .def("get_w_method_test_set", &Dfa::get_w_method_test_set, "Return a W-METHOD test set of strings for current DFA.",py::arg("target_dfa"), py::arg("sigma") = 1)
+      .def("get_w_method_statistics", &Dfa::get_w_method_statistics, "Return a 9-dimensional array with all the w-method statistics.")
+      
+      //===========================================================================================
+
+      //*** Structural similarity ***
+      .def("neighbour_matching_structural_similarity", &Dfa::neighbour_matching_structural_similarity, "Gives the structural similarity score matrix between every pair of states of two DFAs",py::arg("target_dfa"), py::arg("eps") = 0.0001, py::arg("color") = 0)
+      .def("print_structural_similarity", &Dfa::print_structural_similarity, "Print the matrix containing the similarity score between pair of nodes.")
+      
+      //===========================================================================================
+
+      ;
+
 }

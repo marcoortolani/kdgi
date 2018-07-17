0fafd309c5c85640d2800bbee14068c8669b41d5
giammandro <andrea.giammanco@gmail.com>
2018-07-17 15:56:39 +0200

    Temp

--- py/tests/structsim.py
+++ py/tests/structsim.py
@@ -1,12 +1,12 @@
 from gi_gipy import *
 
-dfa_file1 = '../data/others/five_states_chain.txt'
-dfa_file2 = '../data/others/diamante2.txt'
+dfa_file1 = '../data/others/diamante2.txt'
+dfa_file2 = '../data/others/diamante_senza_giri.txt'
 
 dfa1 = Dfa.read_dfa_file(dfa_file1)
 dfa2 = Dfa.read_dfa_file(dfa_file2)
 
 # neighbour_matching_structural_similarity(ConcreteDfa* subject_dfa, double eps=0.0001, bool color=false)
-struct_sim_matrix = dfa1.neighbour_matching_structural_similarity(dfa2, 0.01)
+struct_sim_matrix = dfa1.neighbour_matching_structural_similarity(dfa2, 0.00001)
 
 dfa1.print_structural_similarity(struct_sim_matrix)

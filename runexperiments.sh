ba7a26ec5cdf4795a37f59de4be4bff3f0941e5e
giammandro <andrea.giammanco@gmail.com>
2018-07-29 12:16:10 +0200

    New exp

--- runexperiments.sh
+++ runexperiments.sh
@@ -3,13 +3,8 @@
 # per lanciare da remoto:
 # ssh kdgi@147.163.5.232 screen -d -m ./runexperiments.sh
 
-<<<<<<< HEAD
-cd /home/kdgi
-cd /kdgi/py/tests
-=======
 cd kdgi/py/tests
 
->>>>>>> 0f1ba218728be9e6db7f225c104343c2846b19fd
 
 
 expname="exp-abbadingo_struct_sim"
@@ -26,14 +21,9 @@ echo "START " >> exp_out/test.out
   
 outputfile=exp_out/${expname}.out
 
-<<<<<<< HEAD
-workon kdgi-py2
-python abbadingo_intra_n_struct_sim.py > ${outputfile} 2>> error.out
-=======
 source /home/kdgi/virtualenvs/kdgi-py2/bin/activate kdgi-py2
 
 python abbadingo_intra_n_struct_sim.py > ${outputfile} 2>> exp_out/error.out
->>>>>>> 0f1ba218728be9e6db7f225c104343c2846b19fd
 
 echo "END" >> exp_out/test.out
 

c9746bf15ad548fa681f995d8cb3ab01e129ca57
Mk1092 <marco.curaba@community.unipa.it>
2019-02-20 19:52:04 +0100

    Modified build.sh.

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ fi
 
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -Dtest=on -DPYTHON_LIBRARY=/home/kdgi/anaconda3/envs/alearn/lib/libpython3.6m.so -DPYTHON_INCLUDE_DIR=/home/kdgi/anaconda3/envs/alearn/include/python3.6m -Dpyembed=on  ..
+cmake -G "Unix Makefiles" -Dpython=on -Dpyembed=on -Dtest=on -DPYTHON_LIBRARY=/home/kdgi/anaconda3/envs/alearn/lib/libpython3.6m.so -DPYTHON_INCLUDE_DIR=/home/kdgi/anaconda3/envs/alearn/include/python3.6m ..
 #cmake -G "Unix Makefiles" -Dpyembed=on ..
 make
